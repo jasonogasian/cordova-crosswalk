4e8c9718e7b5b95815acf7185cab7c2ae312f021
Jason Ogasian <jason.ogasian@gmail.com>
2014-12-26 23:24:53 -0800

    Recompiled for ARM.

---
+++ CordovaLib/xwalk_core_library/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jasonogasian/Crosswalk/crosswalk-cordova-9.38.208.10-x86/framework/xwalk_core_library/bin/AndroidManifest.xml : \
+/Users/jasonogasian/Crosswalk/crosswalk-cordova-9.38.208.10-x86/framework/xwalk_core_library/AndroidManifest.xml \
